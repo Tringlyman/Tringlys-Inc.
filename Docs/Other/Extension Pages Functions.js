6ad8692e6508e88e4fa0d3186746b7099ad9f637
Tringlyman <tringlyman@gmail.com>
2025-07-04 22:15:27 +0300

    Update Extension Pages Functions.js

--- Docs/Other/Extension Pages Functions.js
+++ Docs/Other/Extension Pages Functions.js
@@ -3,5 +3,5 @@ function CopyClipboard(text) {
 }
 
 function Docs(text) {
-     const Window = window.open(`https://github.com/Tringlyman/docs.Scratch-Extensions/blob/main/Docs/Other/Docs/${text}.md/`)
+     const Window = window.open(`https://github.com/Tringlyman/docs.Scratch-Extensions/blob/main/Docs/Other/Docs/${text}.md`)
 }
