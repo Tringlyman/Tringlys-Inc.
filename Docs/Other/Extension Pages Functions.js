2b7b3a1a4323bfb60e8c11f0bf86db8e9fd551e9
Tringlyman <tringlyman@gmail.com>
2025-07-04 22:03:43 +0300

    Create Extension Pages Functions.js

---
+++ Docs/Other/Extension Pages Functions.js
@@ -0,0 +1,7 @@
+function CopyClipboard(text) {
+    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`)
+}
+
+function Docs(text) {
+     const Window = window.open(`https://github.com/Tringlyman/docs.Scratch-Extensions/blob/main/Docs/Other/${text}.md/`)
+}
